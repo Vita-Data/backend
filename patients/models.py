296aa256fd9e0941ae418eae29e63df3a6c52dd0
Shaswat098 <shashwatdubey477@gmail.com>
2025-06-19 22:37:57 +0530

    Created ORM Model

--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model):
+    name = models.CharField(max_length = 255)
+    dob = models.DateField()
+    gender = models.CharField(max_length=10)
+    contact_number = models.CharField(max_length=20)
+    email = models.EmailField(unique = True)
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
