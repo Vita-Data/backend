6c46a9f769522c77baee5e1c619b78634772587f
ivan <ivanjaison@gmail.com>
2025-06-30 21:29:02 +0530

    Update: Change db from sqlite to postgres
    Changing from dev(sqlite) to prod ready(Postgres)

--- healthcare/settings.py
+++ healthcare/settings.py
@@ -96,10 +96,15 @@ WSGI_APPLICATION = 'healthcare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myprojectdb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
