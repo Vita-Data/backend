dfba54c3b7ac965a5cf33bb13a665d814de381a4
rdx40 <ivanjaison@gmail.com>
2025-07-27 22:15:32 +0530

    Fix lab reports permissions - allow admin and lab staff access

--- lab_reports/views.py
+++ lab_reports/views.py
@@ -2,14 +2,17 @@ from rest_framework import viewsets, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Patient, LabTestReport
 from .serializers import PatientSerializer, LabTestReportSerializer
+from .permissions import LabReportPermission
 
 class PatientViewSet(viewsets.ModelViewSet):
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
+    permission_classes = [LabReportPermission]
 
 class LabTestReportViewSet(viewsets.ModelViewSet):
     queryset = LabTestReport.objects.all()
     serializer_class = LabTestReportSerializer
+    permission_classes = [LabReportPermission]
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['report_date', 'status', 'patient']
     search_fields = ['patient__name', 'test_type']
